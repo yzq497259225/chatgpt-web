38313aba38b115071680f99e2b5ccd2d6892e31b
yezequan <yezequan@360.cn>
2023-03-31 12:28:57 +0800

    add redis to check authCode

--- service/src/index.ts
+++ service/src/index.ts
@@ -5,88 +5,81 @@ import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
 import { auth } from './middleware/auth'
 import { limiter } from './middleware/limiter'
 import { isNotEmptyString } from './utils/is'
+import redis from 'redis'
+import { promisify } from 'util'
 
 const app = express()
 const router = express.Router()
+
+const client = redis.createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, password: process.env.REDIS_PWD })
+const getAsync = promisify(client.get).bind(client)
 
 app.use(express.static('public'))
 app.use(express.json())
 
 app.all('*', (_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'authorization, Content-Type')
-  res.header('Access-Control-Allow-Methods', '*')
-  next()
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'authorization, Content-Type')
+	res.header('Access-Control-Allow-Methods', '*')
+	next()
 })
 
 router.post('/chat-process', [auth, limiter], async (req, res) => {
-  res.setHeader('Content-type', 'application/octet-stream')
+	res.setHeader('Content-type', 'application/octet-stream')
 
-  try {
-    const { prompt, options = {}, systemMessage } = req.body as RequestProps
-    let firstChunk = true
-    await chatReplyProcess({
-      message: prompt,
-      lastContext: options,
-      process: (chat: ChatMessage) => {
-        res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
-        firstChunk = false
-      },
-      systemMessage,
-    })
-  }
-  catch (error) {
-    res.write(JSON.stringify(error))
-  }
-  finally {
-    res.end()
-  }
+	try {
+		const { prompt, options = {}, systemMessage } = req.body as RequestProps
+		let firstChunk = true
+		await chatReplyProcess({
+			message: prompt,
+			lastContext: options,
+			process: (chat: ChatMessage) => {
+				res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
+				firstChunk = false
+			},
+			systemMessage,
+		})
+	}
+	catch (error) {
+		res.write(JSON.stringify(error))
+	}
+	finally {
+		res.end()
+	}
 })
 
 router.post('/config', auth, async (req, res) => {
-  try {
-    const response = await chatConfig()
-    res.send(response)
-  }
-  catch (error) {
-    res.send(error)
-  }
+	try {
+		const response = await chatConfig()
+		res.send(response)
+	}
+	catch (error) {
+		res.send(error)
+	}
 })
 
 router.post('/session', async (req, res) => {
-  try {
-    const AUTH_SECRET_KEYS = process.env.AUTH_SECRET_KEYS
-    const hasAuth = isNotEmptyString(AUTH_SECRET_KEYS)
-    res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
-  }
-  catch (error) {
-    res.send({ status: 'Fail', message: error.message, data: null })
-  }
+	try {
+		const AUTH_SECRET_KEYS = process.env.AUTH_SECRET_KEYS
+		const hasAuth = isNotEmptyString(AUTH_SECRET_KEYS)
+		res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
+	}
+	catch (error) {
+		res.send({ status: 'Fail', message: error.message, data: null })
+	}
 })
 
 router.post('/verify', async (req, res) => {
-  try {
-    const { token } = req.body as { token: string }
-    if (!token)
-      throw new Error('Secret key is empty')
-
-    // if (process.env.AUTH_SECRET_KEY !== token)
-    //   throw new Error('密钥无效 | Secret key is invalid')
-		let ok = false
-		for (const key of process.env.AUTH_SECRET_KEYS.split(',')) {
-			if (token == key) {
-				ok = true
-				break
-			}
-		}
-		if (!ok)
-			throw new Error('密钥无效 | Secret key is invalid')
-
-    res.send({ status: 'Success', message: 'Verify successfully', data: null })
-  }
-  catch (error) {
-    res.send({ status: 'Fail', message: error.message, data: null })
-  }
+	try {
+		const { token } = req.body as { token: string }
+		if (!token)
+			throw new Error('Secret key is empty')
+		const AUTH_SECRET_KEY = await getAsync(token)
+		if (!AUTH_SECRET_KEY) throw new Error('密钥无效 | Secret key is invalid')
+		res.send({ status: 'Success', message: 'Verify successfully', data: null })
+	} catch (error) {
+		res.send({ status: 'Fail', message: error.message, data: null })
+	}
 })
 
 app.use('', router)
--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -1,26 +1,19 @@
-import { isNotEmptyString } from '../utils/is'
+import redis from 'redis'
+import { promisify } from 'util'
+
+const client = redis.createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, password: process.env.REDIS_PWD })
+const getAsync = promisify(client.get).bind(client)
 
 const auth = async (req, res, next) => {
-	const AUTH_SECRET_KEYS = process.env.AUTH_SECRET_KEYS.split(',')
-	if (AUTH_SECRET_KEYS.length > 0) {
-		try {
-			const Authorization = req.header('Authorization')
-			let isAuthorized = false
-			for (const key of AUTH_SECRET_KEYS) {
-				if (isNotEmptyString(key) && Authorization && Authorization.replace('Bearer ', '').trim() === key.trim()) {
-					isAuthorized = true
-					break
-				}
-			}
-			if (!isAuthorized) throw new Error('Error: 无访问权限 | No access rights')
-			next()
-		}
-		catch (error) {
-			res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
-		}
-	}
-	else {
+	try {
+		const Authorization = req.header('Authorization')
+		if (!Authorization) throw new Error('Error: 无访问权限 | No access rights')
+		const AUTH_SECRET_KEY = await getAsync(Authorization.replace('Bearer ', '').trim())
+		console.log("authKey: ", Authorization.replace('Bearer ', '').trim())
+		if (!AUTH_SECRET_KEY) throw new Error('Error: 无访问权限 | No access rights')
 		next()
+	} catch (error) {
+		res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
 	}
 }
 
